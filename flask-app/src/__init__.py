403ff5b9826f10f77b55fe0cd4b6b907ed1acb9a
ksidarth <ksidarth16@gmail.com>
2023-04-20 15:25:01 -0400

    updated init

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -1,7 +1,7 @@
 # Some set up for the application 
 
 from flask import Flask
-from flaskext.mysql import MySQL
+from flask.ext.mysql import MySQL
 
 # create a MySQL object that we will use in other parts of the API
 db = MySQL()
@@ -31,12 +31,12 @@ def create_app():
 
     # Import the various routes
     from src.views import views
-    from src.customers.customers import customers
+    from src.customers.users import users
     from src.products.products  import products
 
     # Register the routes that we just imported so they can be properly handled
     app.register_blueprint(views,       url_prefix='/v')
-    app.register_blueprint(customers,   url_prefix='/c')
+    app.register_blueprint(users,   url_prefix='/c')
     app.register_blueprint(products,    url_prefix='/p')
 
     return app
