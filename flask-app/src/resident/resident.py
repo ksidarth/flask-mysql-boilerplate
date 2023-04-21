54282149e2f39b8a8992c9de2dba425948efc8a5
AkshayTolani <107060419+AkshayTolani@users.noreply.github.com>
2023-04-20 22:00:09 -0400

    new edits

--- flask-app/src/resident/resident.py
+++ flask-app/src/resident/resident.py
@@ -8,27 +8,27 @@ def execute_command(command):
     cursor.execute(command)
     row_headers = [x[0] for x in cursor.description]
     json_data = []
-    the_data = cursor.fetchall()
-    for row in the_data:
+    theData = cursor.fetchall()
+    for row in theData:
         json_data.append(dict(zip(row_headers, row)))
     return json_data
 
 
 def json_response(json_data):
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
+    response = make_response(jsonify(json_data))
+    response.status_code = 200
+    response.mimetype = 'application/json'
+    return response
 
 
 # get all resident
 @resident.route('/', methods=['GET'])
 def get_residents():
-    json_data_resident = execute_command('select * from resident')
-    for resident in json_data_resident:
-        json_data_resident = execute_command('select * from residentWHERE username="{0}"'.format(resident['username']))
-        resident.update(json_data_resident[0])
-    return json_response(json_data_resident)
+    json_resident = execute_command('select * from resident')
+    for resident in json_resident:
+        json_resident = execute_command('select * from residentWHERE username="{0}"'.format(resident['username']))
+        resident.update(json_resident[0])
+    return json_response(json_resident)
 
 
 # get all resident
@@ -40,51 +40,51 @@ def get_all_residents():
 # add a resident
 @resident.route('/resident', methods=['POST'])
 def post_resident():
-    the_data = request.json
-    current_app.logger.info(the_data)
+    theData = request.json
+    current_app.logger.info(theData)
 
-    username = the_data['username']
-    first_name = the_data['first_name']
-    last_name = the_data['last_name']
-    email = the_data['email']
-    bio = the_data['bio']
-    password = the_data[password]
-    dateAvailabletoBeginSublet = the_data[dateAvailabletoBeginSublet]
-    dateAvailabletoEndSublet = the_data[dateAvailabletoEndSublet]
-    age = the_data[age]
-    requestID = the_data[requestID]
-    propertyID = the_data[propertyID]
+    username = theData['username']
+    first_name = theData['first_name']
+    last_name = theData['last_name']
+    email = theData['email']
+    bio = theData['bio']
+    password = theData[password]
+    dateAvailabletoBeginSublet = theData[dateAvailabletoBeginSublet]
+    dateAvailabletoEndSublet = theData[dateAvailabletoEndSublet]
+    age = theData[age]
+    requestID = theData[requestID]
+    propertyID = theData[propertyID]
 
     cursor = db.get_db().cursor()
     cursor.execute('insert into resident (username, first_name, last_name, email, bio, password, dateAvailabletoBeginSublet, dateAvailabletoEndSublet, age, requestID, propertyID) values ("{0}", "{1}", {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10}, {11})'
                    .format(username, first_name, last_name, email, bio, password, dateAvailabletoBeginSublet, dateAvailabletoEndSublet, age, requestID, propertyID))
     db.get_db().commit()
-    json_data_resident = execute_command('select * from resident WHERE resident_id={0}'.format(cursor.lastrowid))
-    return json_response(json_data_resident)
+    json_resident = execute_command('select * from resident WHERE resident_id={0}'.format(cursor.lastrowid))
+    return json_response(json_resident)
 
 
 # get a resident based on username
 @resident.route('/<username>', methods=['GET'])
 def get_resident(username):
-    json_data_resident = execute_command('select * from resident WHERE username="{0}"'.format(username))[0]
-    json_data_resident.update(execute_command('select * from subletRequest WHERE username="{0}"'.format(username))[0])
-    return json_response(json_data_resident)
+    json_resident = execute_command('select * from resident WHERE username="{0}"'.format(username))[0]
+    json_resident.update(execute_command('select * from subletRequest WHERE username="{0}"'.format(username))[0])
+    return json_response(json_resident)
 
 
 # update a resident's biography
 @resident.route('/<username>/bio', methods=['PUT'])
 def put_bio(username):
-    the_data = request.json
-    current_app.logger.info(the_data)
+    theData = request.json
+    current_app.logger.info(theData)
 
-    bio = the_data['bio']
+    bio = theData['bio']
 
     cursor = db.get_db().cursor()
     cursor.execute('update resident biography="{0}" WHERE username="{1}"'.format(bio, username))
     db.get_db().commit()
-    json_data_resident = execute_command('select * from resident WHERE username="{0}"'.format(username))[0]
-    json_data_resident.update(execute_command('select * from subletRequest WHERE username="{0}"'.format(username))[0])
-    return json_response(json_data_resident)
+    json_resident = execute_command('select * from resident WHERE username="{0}"'.format(username))[0]
+    json_resident.update(execute_command('select * from subletRequest WHERE username="{0}"'.format(username))[0])
+    return json_response(json_resident)
 
 # delete a resident based on resident username
 @resident.route('/resident/<id>', methods=['DELETE'])
@@ -105,28 +105,28 @@ def get_resident_emails(username):
 # update a resident's email
 @resident.route('/<username>/email', methods=['PUT'])
 def put_email(username):
-    the_data = request.json
-    current_app.logger.info(the_data)
+    theData = request.json
+    current_app.logger.info(theData)
 
-    bio = the_data['bio']
+    bio = theData['bio']
 
     cursor = db.get_db().cursor()
     cursor.execute('update residentset biography="{0}" WHERE username="{1}"'.format(bio, username))
     db.get_db().commit()
-    json_data_resident = execute_command('select * from resident WHERE username="{0}"'.format(username))[0]
-    json_data_resident.update(execute_command('select * from subletRequest WHERE username="{0}"'.format(username))[0])
-    return json_response(json_data_resident)
+    json_resident = execute_command('select * from resident WHERE username="{0}"'.format(username))[0]
+    json_resident.update(execute_command('select * from subletRequest WHERE username="{0}"'.format(username))[0])
+    return json_response(json_resident)
 
 # update a resident's dateAvailabletoBeginSublet
 @resident.route('/<username>/dateAvailabletoBeginSublet', methods=['PUT'])
 def put_date(username):
-    the_data = request.json
-    current_app.logger.info(the_data)
+    theData = request.json
+    current_app.logger.info(theData)
 
-    dateAvailabletoBeginSublet = the_data['dateAvailabletoBeginSublet']
+    dateAvailabletoBeginSublet = theData['dateAvailabletoBeginSublet']
 
     cursor = db.get_db().cursor()
     cursor.execute('update residentset biography="{0}" WHERE username="{1}"'.format(dateAvailabletoBeginSublet, username))
     db.get_db().commit()
-    json_data_resident = execute_command('select * from resident WHERE username="{0}"'.format(username))[0]
-    json_data_resident.update(execute_command('select * from subletRequest WHERE username="{0}"'.format(username))[0])
+    json_resident = execute_command('select * from resident WHERE username="{0}"'.format(username))[0]
+    json_resident.update(execute_command('select * from subletRequest WHERE username="{0}"'.format(username))[0])
